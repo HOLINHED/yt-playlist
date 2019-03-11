7f09796c0a2722c97cd441637e56ed01830e6530
Dawid Layne <klatkiewiczdawid@gmail.com>
2019-03-11 12:36:31 -0400

    Added video search

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (err) {
    return;
 }
    // Authorize a client with the loaded credentials, then call the YouTube API.
-   authorize(JSON.parse(content), getChannel);
+   authorize(JSON.parse(content), makePlaylist);
 
 });
     
@@ -106,30 +106,43 @@ function storeToken(token) {
 *
 * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
 */
-function getChannel(auth) {
+function makePlaylist(auth) {
 
-   const service = google.youtube('v3');
+   const Youtube = google.youtube('v3');
 
-   service.channels.list({
-      auth: auth,
-      part: 'snippet,contentDetails,statistics',
-      forUsername: 'GoogleDevelopers'
-   }, (err, response) => {
-      if (err) {
-         console.log('The API returned an error: ' + err);
-         return;
-      }
-
-      const channels = response.data.items;
-
-      if (channels.length == 0) {
-         console.log('No channel found.');
-      } else {
-         console.log('This channel\'s ID is %s. Its title is \'%s\', and ' +
-                     'it has %s views.',
-                     channels[0].id,
-                     channels[0].snippet.title,
-                     channels[0].statistics.viewCount);
+   // Search query for videos. "|" = OR   "-" = NOT
+   const query = "study music | zen music -livestream -live";
+   // Initial search for videos
+   Youtube.search.list({
+      part: 'snippet',
+      q: query,
+      safeSearch: 'strict',
+      maxResults: 5,
+      type: 'video'
+   }, function (err, data) {
+      
+      if (err) console.error('Error: ' + err);
+      
+      if (data) {
+      //console.log(data.data.items)
+      let IDS = "";
+      // Stores video ids
+      data.data.items.forEach(element => IDS += `${element.id.videoId},`);
+         Youtube.videos.list({
+            id: IDS,
+            part: 'contentDetails',
+         }, function(err, data) {
+            if (err) console.error(err);
+            if (data) {
+               
+               for (let video of data.data.items) {
+                  //console.log(video);
+                  console.log(`ID: ${video.id}\nDURATION: ${video.contentDetails.duration}\n`);
+               }
+            }
+         });
+         
       }
    });
+
 }
